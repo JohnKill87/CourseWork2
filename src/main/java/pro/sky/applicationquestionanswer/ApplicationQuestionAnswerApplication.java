b0bb2b97c18c9806e46adea16b3fe8cec3329278
JohnKill87 <yankin.ilja2012@yandex.ru>
2023-09-03 16:39:34 +0300

    Курсовая работа 2 - го курса

---
+++ src/main/java/pro/sky/applicationquestionanswer/ApplicationQuestionAnswerApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.applicationquestionanswer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationQuestionAnswerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationQuestionAnswerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/applicationquestionanswer/ApplicationQuestionAnswerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.applicationquestionanswer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationQuestionAnswerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
